84a94dee3972376f1939256c072bc68db115ccb3
Joshua Ibrom (sk3p7ic) <joshuaibrom@gmail.com>
2023-11-18 12:37:09 -0600

    Refactor AoC21D3 to be more efficient.

--- 2021/Rust/day03/src/bin/main.rs
+++ 2021/Rust/day03/src/bin/main.rs
@@ -2,6 +2,6 @@ use day03::{process_p1, process_p2};
 
 fn main() {
     let input = std::fs::read_to_string("input.txt").expect("file to exist");
-    println!("D3P1: {}", process_p1(&input).power);
+    println!("D3P1: {}", process_p1(&input));
     println!("D3P1: {}", process_p2(&input));
 }
--- 2021/Rust/day03/src/lib.rs
+++ 2021/Rust/day03/src/lib.rs
@@ -1,26 +1,8 @@
-pub struct P1Result {
-    pub power: u32,
-    pub mcb: u16,
-    pub lcb: u16
-}
-
-pub fn process_p1(input: &str) -> P1Result {
+pub fn process_p1(input: &str) -> u32 {
     let lines: Vec<String> = to_line_vec(input);
     let mut most_common_str: String = String::new();
     for i in 0..lines[0].len() {
-        let mut zero_one_counts = (0, 0);
-        for bs in &lines {
-            match bs.chars().nth(i).expect("character to exist") {
-                '0' => zero_one_counts = (zero_one_counts.0 + 1, zero_one_counts.1),
-                '1' => zero_one_counts = (zero_one_counts.0, zero_one_counts.1 + 1),
-                n => panic!("Recieved '{}' when 0 or 1 expected.", n)
-            }
-        }
-        if zero_one_counts.0 > zero_one_counts.1 {
-            most_common_str += "0";
-        } else {
-            most_common_str += "1";
-        }
+        most_common_str += most_common_at_index(&lines, i).to_string().as_str();
     }
     let mut lcb_mask = String::new();
     for i in 0..16 {
@@ -34,7 +16,7 @@ pub fn process_p1(input: &str) -> P1Result {
     let mcb = u16::from_str_radix(&most_common_str, 2).expect("string to be bits");
     let mut lcb = !mcb;
     lcb &= lcb_mask;
-    return P1Result{power: (mcb as u32) * (lcb as u32), mcb, lcb};
+    return (mcb as u32) * (lcb as u32);
 }
 
 pub fn process_p2(input: &str) -> u32 {
@@ -48,9 +30,7 @@ pub fn process_p2(input: &str) -> u32 {
                 vline += line.as_str();
                 vline += "\n";
             }
-            let res = process_p1(vline.as_str());
-            let mcb = strip_start(format!("{:016b}", res.mcb).as_str(), 16-mcb_lines.get(0).unwrap().len());
-            let bit = mcb.chars().nth(i).expect("char to exist");
+            let bit = most_common_at_index(&to_line_vec(vline.as_str()), i);
             mcb_lines = mcb_lines.into_iter().filter(|s| s.chars().nth(i).unwrap() == bit).collect();
             if mcb_lines.len() == 1 {
                 break;
@@ -65,9 +45,11 @@ pub fn process_p2(input: &str) -> u32 {
                 vline += line.as_str();
                 vline += "\n";
             }
-            let res = process_p1(vline.as_str());
-            let lcb = strip_start(format!("{:016b}", res.lcb).as_str(), 16-lcb_lines.get(0).unwrap().len());
-            let bit = lcb.chars().nth(i).expect("char to exist");
+            let bit = match most_common_at_index(&to_line_vec(vline.as_str()), i) {
+                '0' => '1',
+                '1' => '0',
+                n => panic!("Recieved '{}' when 0 or 1 expected.", n)
+            };
             lcb_lines = lcb_lines.into_iter().filter(|s| s.chars().nth(i).unwrap() == bit).collect();
             if lcb_lines.len() == 1 {
                 break;
@@ -81,62 +63,26 @@ pub fn process_p2(input: &str) -> u32 {
     return mcb * lcb;
 }
 
-pub fn old_process_p2(input: &str, mcb: u16, lcb: u16) -> u32 {
-    let mut mcb_lines = to_line_vec(&input);
-    let mut lcb_lines = to_line_vec(&input);
-    let mut mcb_str = format!("{:016b}", mcb);
-    let mut lcb_str = format!("{:016b}", lcb);
-    mcb_str = strip_start(&mcb_str, 16 - mcb_lines[0].len());
-    lcb_str = strip_start(&lcb_str, 16 - mcb_lines[0].len());
-    for i in 0..mcb_str.len() {
-        let mut removable_indices: Vec<usize> = Vec::new();
-        let mut offset = 0;
-        for j in 0..mcb_lines.len() {
-            let bs = &mcb_lines.get(j).unwrap();
-            if removable_indices.len() < mcb_lines.len() - 1 &&
-                bs.chars().nth(i).unwrap() != mcb_str.chars().nth(i).unwrap() {
-                removable_indices.push(j - offset);
-                offset += 1;
-            }
-        }
-        for idx in removable_indices {
-            mcb_lines.remove(idx);
-        }
-    }
-    for i in 0..lcb_str.len() {
-        let mut removable_indices: Vec<usize> = Vec::new();
-        let mut offset = 0;
-        for j in 0..lcb_lines.len() {
-            let bs = &lcb_lines.get(j).unwrap();
-            if removable_indices.len() < lcb_lines.len() - 1 &&
-                bs.chars().nth(i).unwrap() != lcb_str.chars().nth(i).unwrap() {
-                removable_indices.push(j - offset);
-                offset += 1;
-            }
-        }
-        for idx in removable_indices {
-            lcb_lines.remove(idx);
-        }
-    }
-    let mcs = mcb_lines.get(0).expect("vec to have single value");
-    let lcs = lcb_lines.get(0).expect("vec to have single value");
-    let mcb = u32::from_str_radix(mcs, 2).expect("string to be bits");
-    let lcb = u32::from_str_radix(lcs, 2).expect("string to be bits");
-    return mcb * lcb;
-}
-
 fn to_line_vec(input: &str) -> Vec<String> {
     input.trim().lines()
         .map(|s| s.to_string())
         .collect()
 }
 
-fn strip_start(input: &str, n: usize) -> String {
-    let mut res = String::new();
-    for i in n..input.len() {
-        res.push(input.chars().nth(i).expect("character to exist"));
+fn most_common_at_index(lines: &Vec<String>, index: usize) -> char {
+    let mut zero_one_counts = (0, 0);
+    for bs in lines {
+        match bs.chars().nth(index).expect("character to exist") {
+            '0' => zero_one_counts = (zero_one_counts.0 + 1, zero_one_counts.1),
+            '1' => zero_one_counts = (zero_one_counts.0, zero_one_counts.1 + 1),
+            n => panic!("Recieved '{}' when 0 or 1 expected.", n)
+        }
     }
-    return res;
+    if zero_one_counts.0 > zero_one_counts.1 {
+        return '0';
+    } else {
+        return '1';
+    }
 }
 
 #[cfg(test)]
@@ -148,7 +94,7 @@ mod tests {
     #[test]
     fn p1_works() {
         let res = process_p1(&SAMPLE);
-        assert_eq!(res.power, 198);
+        assert_eq!(res, 198);
     }
 
     #[test]
