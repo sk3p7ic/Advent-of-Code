915f170dd8e4b549cf677a612d7af2a5e53859ad
Joshua Ibrom <joshuaibrom@gmail.com>
2023-11-20 13:16:19 -0600

    AoC Day 06 in Rust solved.

--- 2021/Rust/day06/src/lib.rs
+++ 2021/Rust/day06/src/lib.rs
@@ -4,24 +4,30 @@ pub fn process_p1(input: &str) -> usize {
     let n_each_fish = vec![0, 1, 2, 3, 4, 5, 6, 7, 8].iter()
         .map(|&f| starting_fish.iter().filter(|&fv| *fv == f).count())
         .collect::<Vec<usize>>();
-    let mut total_fish = 0usize;
-    for i in 0..8 {
-        total_fish += n_each_fish.get(i).expect("count to exist") * fish_counts_after_days.get(i).expect("fish count to exist");
-    }
-    total_fish
+    n_each_fish.iter()
+        .enumerate()
+        .map(|(i, n)| n * fish_counts_after_days.get(i).expect("count to exist"))
+        .sum()
 }
 
 pub fn process_p2(input: &str) -> usize {
     let starting_fish = parse_input(input);
-    let fish_counts_after_days = get_all_fish_counts(256);
-    let n_each_fish = vec![0, 1, 2, 3, 4, 5, 6, 7, 8].iter()
-        .map(|&f| starting_fish.iter().filter(|&fv| *fv == f).count())
-        .collect::<Vec<usize>>();
-    let mut total_fish = 0usize;
-    for i in 0..8 {
-        total_fish += n_each_fish.get(i).expect("count to exist") * fish_counts_after_days.get(i).expect("fish count to exist");
+    // Thanks to https://github.com/duarten for aiding me in this issue
+    // and letting me know that my original idea was overthinking things
+    let mut counts: [usize; 9] = [0; 9];
+    starting_fish.iter()
+        .for_each(|f| counts[*f] += 1);
+    for _ in 0..256 {
+        for (i, f) in std::mem::take(&mut counts).into_iter().enumerate() {
+            if i == 0 {
+                counts[6] += f;
+                counts[8] += f;
+            } else {
+                counts[i-1] += f;
+            }
+        }
     }
-    total_fish
+    counts.into_iter().sum()
 }
 
 fn parse_input(input: &str) -> Vec<usize> {
@@ -46,8 +52,8 @@ fn simulate_fish_from_day(starting_day: i8, n_days: usize) -> usize {
     fish_vec.len()
 }
 
-fn get_all_fish_counts(n_days: usize) -> [usize; 8] {
-    let mut all_counts = [0; 8];
+fn get_all_fish_counts(n_days: usize) -> [usize; 9] {
+    let mut all_counts = [0; 9];
     for d in 0..all_counts.len() {
         all_counts[d] = simulate_fish_from_day(d as i8, n_days);
     }
@@ -68,5 +74,10 @@ mod tests {
     #[test]
     fn p1_works() {
         assert_eq!(process_p1(&SAMPLE), 5934);
+    }
+
+    #[test]
+    fn p2_works() {
+        assert_eq!(process_p2(&SAMPLE), 26984457539);
     }
 }
