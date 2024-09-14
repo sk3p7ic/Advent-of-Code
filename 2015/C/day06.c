3312ae70e2590e4133087a41c0b52bb2316702a0
Joshua Ibrom <joshuaibrom@gmail.com>
2024-09-14 11:33:45 -0500

    AoC 2015 Day 06 solved.
    I believe there was an issue with pointers or something to how I was
    storing data in the original 2D input buffer. After some slight
    refactoring everything worked.

--- 2015/C/day06.c
+++ 2015/C/day06.c
@@ -3,13 +3,12 @@
 #include <string.h>
 
 #define N_ROWS 300
-#define N_COLS 34
-#define GRID_SZ 1000 * 1000
+#define N_COLS 33
 
 void panic(const char* msg);
 void expect(void* ptr, const char* msg);
 
-void readFile(const char* filename, char buf[N_ROWS][N_COLS]);
+void readFile(const char* filename, char* buf);
 
 
 typedef enum { CT_ON, CT_OFF, CT_TOG } CommandType;
@@ -25,72 +24,43 @@ typedef struct {
     Coord c2;
 } Command;
 
-Coord parseCoord(const char* str) {
-    // Create dynamic input buffer
-    char* input = malloc(strlen(str));
-    expect(input, "Cannot create buffer to parse coordinate.");
-    strcpy(input, str);
-
-    // Parse the input
-    Coord c;
-    char *tok = malloc(3);
-    expect(tok, "Cannot create coordinate parsing token buffer.");
-
-    tok = strtok(input, ",");
-    expect(tok, "Coordinate 'x' cannot be NULL.");
-    c.x = atoi(tok);
-    tok = strtok(NULL, ",");
-    expect(tok, "Coordinate 'y' cannot be NULL.");
-    c.y = atoi(tok);
-
-    free(input);
-
-    return c;
-}
-
 Command parseLine(const char* line) {
-    // Create a dynamic input
-    char* input = malloc(strlen(line));
-    expect(input, "Cannot create buffer to parse input line.");
-    strcpy(input, line);
-    
-    // Parse the input
     Command cmd = {};
-    char* tok = malloc(7);
-    expect(tok, "Cannot create token buffer.");
-    char* c1Str = malloc(7);
-    char* c2Str = malloc(7);
-    expect(c1Str, "Cannot create buffer for Coordinate 1 string.");
-    expect(c1Str, "Cannot create buffer for Coordinate 2 string.");
-
-    int parseC2 = 0;
-
-    tok = strtok(input, " ");
-    while (tok != NULL) {
-        int foundCommandToken = 0;
-        if (!strcmp(tok, "turn")) foundCommandToken = 1; // Ignore "turn" token
-        if (!strcmp(tok, "on")) { cmd.ct = CT_ON; foundCommandToken = 1; }
-        if (!strcmp(tok, "off")) { cmd.ct = CT_OFF; foundCommandToken = 1; }
-        if (!strcmp(tok, "toggle")) { cmd.ct = CT_TOG; foundCommandToken = 1; }
-        if (!strcmp(tok, "through")) { parseC2 = 1; foundCommandToken = 1; }
-
-        if (!foundCommandToken) {
-            if (!parseC2) strcpy(c1Str, tok);
-            else strcpy(c2Str, tok);
-        }
-
-        tok = strtok(NULL, " ");
+    // Parse out "turn" string prefix, if present
+    char turnStr[] = "turn";
+    char* input = malloc(strlen(line));
+    expect(input, "Could not allocate input buffer to parse line to Command.");
+    if (!strncmp(line, turnStr, strlen(turnStr))) {
+        strcpy(input, &line[strlen(turnStr) + 1]);
+    } else {
+        strcpy(input, line);
     }
 
-    cmd.c1 = parseCoord(c1Str);
-    cmd.c2 = parseCoord(c2Str);
+    // Define vars to store sccanf results
+    char state[8];
+    Coord c1 = {};
+    Coord c2 = {};
 
-    free(tok);
-    free(c1Str);
-    free(c2Str);
+    // Parse the line
+    sscanf(input, "%s %d,%d through %d,%d", state, &c1.x, &c1.y, &c2.x, &c2.y);
+
+    // Not the "cleanest" way to parse, but it works easily for this case.
+    switch (state[1]) {
+        case 'n': cmd.ct = CT_ON; break;
+        case 'f': cmd.ct = CT_OFF; break;
+        case 'o': cmd.ct = CT_TOG;
+    }
+
+    cmd.c1 = c1;
+    cmd.c2 = c2;
+
     free(input);
 
     return cmd;
+}
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
 }
 
 int processP1(Command* cmds, size_t n_commands) {
@@ -103,8 +73,10 @@ int processP1(Command* cmds, size_t n_commands) {
                 switch (cmd.ct) {
                     case CT_ON:
                         grid[r][c] = 1;
+                        break;
                     case CT_OFF:
                         grid[r][c] = 0;
+                        break;
                     case CT_TOG:
                         grid[r][c] = !grid[r][c];
                 }
@@ -121,26 +93,65 @@ int processP1(Command* cmds, size_t n_commands) {
     return nLit;
 }
 
+int processP2(Command* cmds, size_t n_commands) {
+    short grid[1000][1000] = {0};
+
+    for (size_t i = 0; i < n_commands; ++i) {
+        Command cmd = cmds[i];
+        for (int r = cmd.c1.y; r <= cmd.c2.y; ++r) {
+            for (int c = cmd.c1.x; c <= cmd.c2.x; ++c) {
+                switch (cmd.ct) {
+                    case CT_ON:
+                        grid[r][c] += 1;
+                        break;
+                    case CT_OFF:
+                        grid[r][c] = max(grid[r][c] - 1, 0);
+                        break;
+                    case CT_TOG:
+                        grid[r][c] += 2;
+                }
+            }
+        }
+    }
+
+    int brightness = 0;
+    for (size_t i = 0; i < 1000; ++i) {
+        for (size_t j = 0; j < 1000; ++j) {
+            brightness += grid[i][j];
+        }
+    }
+    return brightness;
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Incorrect number of arguments given (%d).\n", argc);
         fprintf(stderr, "Usage: day06 <input-file>\n");
         exit(EXIT_FAILURE);
     }
-    char buf[N_ROWS][N_COLS];
+    char* buf = malloc(N_ROWS * N_COLS);
+    expect(buf, "Could not allocate buffer to store input file contents.");
     readFile(argv[1], buf);
 
     Command* cmds = malloc(sizeof(Command) * N_ROWS);
     expect(cmds, "Could not allocate buffer to store commands.");
+
+    char* line = malloc(N_COLS);
+    expect(line, "Could not allocate buffer to read input file lines.");
+    line = strtok(buf, "\n");
     for (size_t i = 0; i < N_ROWS; ++i) {
-        cmds[i] = parseLine(buf[i]);
+        cmds[i] = parseLine(line);
+        line = strtok(NULL, "\n");
+        if (line == NULL) {
+            fprintf(stderr, "Got NULL line when not expected (idx=%ld)\n", i);
+            break;
+        }
     }
+    free(buf);
 
     printf("D06P01: %d\n", processP1(cmds, N_ROWS));
+    printf("D06P02: %d\n", processP2(cmds, N_ROWS));
     
-    Command cmdTest[] = { parseLine("turn on 0,0 through 999,999") };
-    printf("cmdTest: %d\n", processP1(cmdTest, 1));
-
     free(cmds);
     return 0;
 }
@@ -154,22 +165,13 @@ void expect(void* ptr, const char* msg) {
     if (ptr == NULL) panic(msg);
 }
 
-void readFile(const char* filename, char buf[N_ROWS][N_COLS]) {
+void readFile(const char* filename, char* buf) {
     FILE* f = fopen(filename, "r");
     expect(f, "Could not open input file.");
 
-    size_t row = 0;
-    size_t col = 0;
-
-    while (!feof(f) && row < N_ROWS) {
-        char c = fgetc(f);
-        if (c == '\r') continue;
-        if (c == '\n') {
-            col = 0;
-            row++;
-            continue;
-        }
-        buf[row][col++] = c;
+    size_t idx = 0;
+    while (!feof(f)) {
+        buf[idx++] = fgetc(f);
     }
 
     fclose(f);
