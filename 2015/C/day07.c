1abbba883f1c2d9776d27e8288df73701c19e1c4
Joshua Ibrom <joshuaibrom@gmail.com>
2024-09-15 17:32:13 -0500

    AoC 2015 Day 07 in-prog.
    Implemented hashing to convert city names to array indicies which are
    used to build a 2D array that will be passed to Prim's algorithm.

---
+++ 2015/C/day07.c
@@ -0,0 +1,188 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void panic(const char* msg);
+void expect(void* ptr, const char* msg);
+
+void readFile(const char* filename, char* buf);
+
+
+size_t n_vertices = 0;
+
+typedef struct {
+    char** items;
+    size_t sz;
+    size_t max_sz;
+} FixedSet;
+
+FixedSet initFixedSet(size_t fixed_size) {
+    char** items = malloc(sizeof(char *) * fixed_size);
+    expect(items, "Could not create items buffer for FixedSet.");
+    FixedSet s = { items, 0, fixed_size };
+    return s;
+}
+
+void addFixedSetItem(char* item, FixedSet* s) {
+    if (s->sz + 1 > s->max_sz) {
+        fprintf(stderr, "Ran out of room in FixedSet (max size %ld)\n", s->sz);
+        panic("Cannot add item to FixedSet.");
+    }
+    for (size_t i = 0; i < s->sz; ++i) {
+        // If the element is already in the FixedSet
+        if (!strcmp(s->items[i], item)) {
+            return;
+        }
+    }
+    s->items[s->sz++] = item;
+}
+
+typedef struct {
+    char* c1;
+    char* c2;
+    int len;
+} Route;
+
+Route parseRoute(const char* s) {
+    Route r = { malloc(32), malloc(32), 0 };
+    char c1[32];
+    char c2[32];
+    sscanf(s, "%s to %s = %d", c1, c2, &r.len);
+    strncpy(r.c1, c1, strlen(c1));
+    strncpy(r.c2, c2, strlen(c2));
+    return r;
+}
+
+typedef struct {
+    Route* routes;
+    size_t nRoutes;
+} RouteGroup;
+
+RouteGroup getRoutes(const char* str) {
+    Route* routes = malloc(sizeof(Route) * 32);
+    expect(routes, "Could not allocate buffer to store Routes.");
+    char* s = malloc(strlen(str));
+    expect(s, "Could not allocate buffer to parse input to Routes.");
+    strncpy(s, str, strlen(str));
+    char* tok = malloc(64);
+    expect(tok, "Could not allocate token buffer to parse lines to Routes.");
+    tok = strtok(s, "\n");
+    size_t nRoutes = 0;
+    while (tok != NULL && strlen(tok) > 1) {
+        routes[nRoutes++] = parseRoute(tok);
+        tok = strtok(NULL, "\n");
+    }
+    RouteGroup rg = { routes, nRoutes };
+    return rg;
+}
+
+typedef struct {
+    int v[50];
+} Hasher;
+
+int hash(char* s) {
+    // We'll ignore the case where strlen(s) < 2
+    return s[0] + s[1] - ('A' + 'a');
+}
+
+Hasher initHasher(char** strs, size_t n_strs) {
+    Hasher h = {};
+    for (size_t i = 0; i < n_strs; ++i) {
+        h.v[hash(strs[i])] = (int) i;
+    }
+    return h;
+}
+
+int hashPos(Hasher h, char* s) {
+    return h.v[hash(s)];
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Incorrect number of arguments given (%d).\n", argc);
+        fprintf(stderr, "Usage: day07 <input-file>\n");
+        exit(EXIT_FAILURE);
+    }
+    {
+        char* routeStr = "London to Dublin = 464\nLondon to Belfast = 518\nDublin to Belfast = 141";
+        RouteGroup routes = getRoutes(routeStr);
+        FixedSet set = initFixedSet(routes.nRoutes + 1);
+        for (size_t i = 0; i < routes.nRoutes; ++i) {
+            addFixedSetItem(routes.routes[i].c1, &set);
+            addFixedSetItem(routes.routes[i].c2, &set);
+        }
+        n_vertices = set.sz;
+        Hasher hasher = initHasher(set.items, n_vertices);
+        for (int i = 0; i < n_vertices; ++i) {
+            printf("%s: %d\n", set.items[i], hashPos(hasher, set.items[i]));
+        }
+        int edges[n_vertices][n_vertices];
+        for (int i = 0; i < n_vertices; ++i) {
+            for (int j = 0; j < n_vertices; ++j) {
+                edges[i][j] = 0;
+            }
+        }
+        for (size_t i = 0; i < routes.nRoutes; ++i) {
+            Route r = routes.routes[i];
+            edges[hashPos(hasher, r.c1)][hashPos(hasher, r.c2)] = r.len;
+        }
+        for (int i = 0; i < n_vertices; ++i) {
+            for (int j = 0; j < n_vertices; ++j) {
+                printf(" %3d", edges[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    char* input = malloc(1024);
+    expect(input, "Could not create buffer for reading input file.");
+    readFile(argv[1], input);
+    RouteGroup routes = getRoutes(input);
+    FixedSet set = initFixedSet(routes.nRoutes + 1);
+    for (size_t i = 0; i < routes.nRoutes; ++i) {
+        addFixedSetItem(routes.routes[i].c1, &set);
+        addFixedSetItem(routes.routes[i].c2, &set);
+    }
+    n_vertices = set.sz;
+    Hasher hasher = initHasher(set.items, n_vertices);
+    for (int i = 0; i < n_vertices; ++i) {
+        printf("%s: %d\n", set.items[i], hashPos(hasher, set.items[i]));
+    }
+    int edges[n_vertices][n_vertices];
+    for (int i = 0; i < n_vertices; ++i) {
+        for (int j = 0; j < n_vertices; ++j) {
+            edges[i][j] = 0;
+        }
+    }
+    for (size_t i = 0; i < routes.nRoutes; ++i) {
+        Route r = routes.routes[i];
+        edges[hashPos(hasher, r.c1)][hashPos(hasher, r.c2)] = r.len;
+    }
+    for (int i = 0; i < n_vertices; ++i) {
+        for (int j = 0; j < n_vertices; ++j) {
+            printf(" %3d", edges[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+void panic(const char* msg) {
+    fprintf(stderr, "%s\n", msg);
+    exit(EXIT_FAILURE);
+}
+
+void expect(void* ptr, const char* msg) {
+    if (ptr == NULL) panic(msg);
+}
+
+void readFile(const char* filename, char* buf) {
+    FILE* f = fopen(filename, "r");
+    expect(f, "Could not open input file.");
+
+    size_t idx = 0;
+    while (!feof(f)) {
+        buf[idx++] = fgetc(f);
+    }
+
+    fclose(f);
+}
